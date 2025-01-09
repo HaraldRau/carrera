7a4b7f74765e6c86389adcb7333783c4177ee2d4
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-01-09 11:45:06 +0100

    Update interruppt.ino
    Interruptschleife bereinigt

--- interruppt.ino
+++ interruppt.ino
@@ -1,43 +1,23 @@
-int running = 0;
-int counter = 0;
-
-//String ausgabe = "";
+bool durchfahrt_1 = 0;
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), blink, FALLING);
-
-  Serial.println(name);
+  attachInterrupt(digitalPinToInterrupt(2), spur_1, FALLING);
 }
 
 
 void loop()
 {
-//Serial.println(digitalRead(2));
-delay(100);
+	Serial.println(durchfahrt_1);
+	delay(100);
 }
 
-void blink()
+void spur_1()
 {
-  if (running > 0) 
-  {
-    return 0;
-  }
-  else
-  {
-    running = 1;
-    String ausgabe = "Counter: ";
-    ausgabe.concat(counter);
-    ausgabe.concat(name);
-    Serial.println(ausgabe);
-    counter++;
-    delay(3000);
-    name = "Harald";
-    running = 0;
-  }
-
+durchfahrt_1 = 1
+return durchfahrt_1
 }
 
 
