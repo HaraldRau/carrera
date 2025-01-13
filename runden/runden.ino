4e69b5c4c8c4227f0dfeed41f06bf40b62190412
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-01-13 17:25:34 +0100

    Trigger
    Trigger verbessert, Werte dürfe alle 10 sec anfallen
    Anzeige in Funktion ausgelagert

--- runden/runden.ino
+++ runden/runden.ino
@@ -1,7 +1,12 @@
+#include <DigitLedDisplay.h>
+
+// initialize with pin numbers for DIN, CS, CLK
+DigitLedDisplay ld = DigitLedDisplay(7, 6, 5);
+
 bool durchfahrt_1 = 0;
 bool durchfahrt_2 = 0;
 int runde = 0;
-int runden_zeit[50];
+int runden_zeit_1[50];
 unsigned long startzeit = millis();
 unsigned long rennzeit = 0;
 
@@ -10,33 +15,31 @@ void setup()
   Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), spur_1, FALLING);
-  Serial.println(startzeit);
+  ld.setBright(15); // range is 0-15
+  ld.setDigitLimit(8);
 }
 
 
 void loop()
 {
-  runden_zeit[0]=startzeit;
-  rennzeit = abs(millis()/10);
-  if (durchfahrt_1 == 1)
+  runden_zeit_1[0]=startzeit;
+  rennzeit = abs(millis()/100);
+  if (durchfahrt_1 == 1 && runden_zeit_1[runde]+100<rennzeit)
   {
-    if (runden_zeit[runde]+150<rennzeit)
-    {
-      runde++;
-      Serial.print("Runde: ");
-      Serial.print(runde);
-      Serial.print(" ");
-      Serial.println(rennzeit);
-      delay(10);
-      durchfahrt_1 = 0;
-      runden_zeit[runde] = rennzeit;
-    }
+    runde++;
+    delay(10);
+    durchfahrt_1 = 0;
+    anzeige_1();   
+  }
+  else
+  {
+    durchfahrt_1 = 0;
   }
 	delay(10);
   if (runde>9)
   {for (int i=0; i< 10; i++)
   {
-    Serial.println(runden_zeit[i]);
+    Serial.println(runden_zeit_1[i]);
     delay(100);
     runde=0;
     }
@@ -47,4 +50,15 @@ void spur_1()
 {
 durchfahrt_1 = 1;
 return durchfahrt_1;
+}
+
+void anzeige_1()
+{
+  runden_zeit_1[runde] = rennzeit;
+  ld.clear();ld.printDigit(runde,4);
+  ld.printDigit(rennzeit,0);
+  Serial.println(rennzeit);
+  //Serial.print("Runde: ");
+  //Serial.print(runde);
+  //Serial.print(" ");
 }
