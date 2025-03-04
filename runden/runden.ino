0e00e7e7444853cc762b9420a7db3b4f7bdd77c2
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2025-03-04 09:41:28 +0100

    Add files via upload

--- runden/runden.ino
+++ runden/runden.ino
@@ -1,15 +1,7 @@
-#include <DigitLedDisplay.h>
-
-// initialize with pin numbers for DIN, CS, CLK
-DigitLedDisplay ld = DigitLedDisplay(7, 6, 5);
-
 bool durchfahrt_1 = 0;
 bool durchfahrt_2 = 0;
-bool start = 0;
-int runde_1 = 0;
-int runde_2 = 0;
-int runden_zeit_1[50];
-int runden_zeit_2[50];
+int runde = 0;
+int runden_zeit[9];
 unsigned long startzeit = millis();
 unsigned long rennzeit = 0;
 
@@ -17,55 +9,23 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(3), spur_1, FALLING);
-  attachInterrupt(digitalPinToInterrupt(2), spur_2, FALLING);
-  ld.setBright(15); // range is 0-15
-  ld.setDigitLimit(8);
-  starten();//Rennen starten und Starzeit auf Runde 0 setzen
+  attachInterrupt(digitalPinToInterrupt(2), spur_1, FALLING);
+  Serial.println(startzeit);
 }
 
 
 void loop()
 {
-  rennzeit = abs(millis()/100)-startzeit;
-  if (durchfahrt_1 == 1 && runden_zeit_1[runde_1]+50<rennzeit)
+  rennzeit = abs(millis()/10);
+  if (durchfahrt_1 == 1)
   {
-    runde_1++;
-    runden_zeit_1[runde_1] = rennzeit;
+    Serial.print("Runde: ");
+    Serial.print(runde);
+    Serial.print(" ");
+    Serial.println(rennzeit);
+    delay(100);
     durchfahrt_1 = 0;
-    // delay(10);
-    anzeige_1();
-  }
-  else
-  {
-    durchfahrt_1 = 0;
-  }
-
-    if (durchfahrt_2 == 1 && runden_zeit_2[runde_2]+50<rennzeit)
-  {
-    runde_2++;
-    runden_zeit_2[runde_2] = rennzeit;
-    durchfahrt_2 = 0;
-    // delay(10);
-    anzeige_2();
-  }
-  else
-  {
-    durchfahrt_2 = 0;
-  }
-
-   if (runde_1 > 9)
-  {
-    for (int i=1; i< 10; i++)
-    {
-      Serial.print(runden_zeit_1[i] - runden_zeit_1[i-1]);
-      Serial.print(" | ");
-      Serial.println(runden_zeit_2[i] - runden_zeit_2[i-1]);
-      delay(100);
-      runde_1=0;
-      runde_2=0;
-    }
+    runde++;
   }
 	delay(10);
 }
@@ -75,60 +35,3 @@ void spur_1()
 durchfahrt_1 = 1;
 return durchfahrt_1;
 }
-
-void spur_2()
-{
-durchfahrt_2 = 1;
-return durchfahrt_2;
-}
-
-void anzeige_1()
-{
-  ld.clear();
-  // ld.printDigit(rennzeit,4);
-  // delay(1500);
-  // ld.clear();
-  ld.printDigit(runde_1,4);
-  ld.printDigit(runde_2,0);
-  // Serial.println(rennzeit);
-}
-
-void anzeige_2()
-{
-  ld.clear();
-  // ld.printDigit(rennzeit,7);
-  // delay(1500);
-  // ld.clear();
-  ld.printDigit(runde_2,0);
-  ld.printDigit(runde_1,4);
-  // Serial.println(rennzeit);
-}
-
-void starten()
-{
-  ld.printDigit(8,0);
-  delay(500);
-  ld.printDigit(88,0);
-  delay(500);
-  ld.printDigit(888,0);
-  delay(500);
-  ld.printDigit(8888,0);
-  delay(500);
-  ld.printDigit(88888,0);
-  delay(500);
-  ld.printDigit(888888,0);
-  delay(500);
-  ld.printDigit(8888888,0);
-  delay(500);
-  ld.printDigit(88888888,0);
-  delay(1000);
-  ld.clear();
-  startzeit = abs(millis()/100);
-  runden_zeit_1[0]=0;
-  runden_zeit_2[0]=0;
-  Serial.println(startzeit);
-  Serial.println("--START--");
-  return;
-}
-void ziel(){}
-void geschwindigkeit(){}
